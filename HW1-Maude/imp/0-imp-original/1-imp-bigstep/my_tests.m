900bb1f0d330d2378e9cc405e15fd2502c743cfa
Brando Miranda <brando90@mit.edu>
2018-09-18 18:51:34 -0500

    my AND goes in

--- HW1-Maude/imp/0-imp-original/1-imp-bigstep/my_tests.m
+++ HW1-Maude/imp/0-imp-original/1-imp-bigstep/my_tests.m
@@ -8,29 +8,57 @@ in ../imp-programs.maude
 
 mod TEST is including IMP-SEMANTICS-BIGSTEP + IMP-PROGRAMS .  endm
 
----var Sigma : State .
+--- set trace on .
 
 ---rewrite < 1 / 0 , .State > .
 ---rewrite < 1 / 1 , .State > .
 
---- Test , # tests 4
 rew < Error > .
-rew < {}, Error > .
-rew < { x = 5 ; } , Error > .
-rew < x = 5 ; , Error > .
 
 --- AExps tests, # tests 5
 rew < 3, .State > .
 rew < 3, Error > .
 rew < 3 / 0 , .State > .
+rew < 1 / 0 , .State > .
 rew < 3 + 3 / 0 , .State > .
 rew < 3 / 0 + 3 , .State > .
 
---- BExps tests # test 3
-rew < 3 / 0 <= 3 , .State > .
-rew < 3 <= 3 / 0 , .State > .
+--- BExps tests # test
 rew < true, Error > .
 rew < 1 ==Bool 1, Error > .
+rew < false, Error > .
+rew < 1 ==Bool 0, Error > .
+
+rew < 3 / 0 <= 3 , .State > .
+rew < 3 <= 3 / 0 , .State > .
+rew < 3 / 0 <= 3 / 0 , .State > .
+rew < 3 <= 3 , Error > .
+
+rew < ! true,.State > .
+rew < ! true,Error > .
+rew < ! false,.State > .
+rew < ! false,Error > .
+rew < ! 1 <= 1,Error > .
+rew < ! 1 / 0 <= 1,Error > .
+
+rew < true && true, .State > .
+rew < true && false, .State > .
+rew < true && true, Error > .
+rew < true && false, Error > .
+rew < false && false, Error > .
+rew < true && 3 / 0 <= 2, .State > . --- < Error >
+rew < 3 / 0 <= 2 && true, .State > . --- < Error >
+
+rew < 3 / 0 <= 2 && false, .State > . --- false
+rew < false && 3 / 0 <= 2, .State > . --- false
+
+--- Test , # tests 4
+rew < {}, Error > .
+
+--- set trace on .
+rew < { x = 5 ; } , .State > .
+rew < { x = 5 ; } , Error > .
+---rew < x = 5 ; , Error > .
 
 ---
 --- rew < x =  3 / 1 ; ,  x |-> 0 > .

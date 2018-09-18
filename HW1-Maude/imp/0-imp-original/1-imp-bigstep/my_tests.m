3cf39e21a117e19302610a04045b8f7dd335bde3
Brando Miranda <brando90@mit.edu>
2018-09-18 16:30:52 -0500

    added more errors

--- HW1-Maude/imp/0-imp-original/1-imp-bigstep/my_tests.m
+++ HW1-Maude/imp/0-imp-original/1-imp-bigstep/my_tests.m
@@ -9,28 +9,33 @@ in ../imp-programs.maude
 mod TEST is including IMP-SEMANTICS-BIGSTEP + IMP-PROGRAMS .  endm
 
 ---var Sigma : State .
---- set trace on .
 
 ---rewrite < 1 / 0 , .State > .
 ---rewrite < 1 / 1 , .State > .
+
+--- Test , # tests 4
 rew < Error > .
 rew < {}, Error > .
 rew < { x = 5 ; } , Error > .
 rew < x = 5 ; , Error > .
 
----rew < 3 + Error , .State > .
+--- AExps tests, # tests 5
+rew < 3, .State > .
+rew < 3, Error > .
 rew < 3 / 0 , .State > .
-
 rew < 3 + 3 / 0 , .State > .
 rew < 3 / 0 + 3 , .State > .
 
+--- BExps tests # test 3
 rew < 3 / 0 <= 3 , .State > .
 rew < 3 <= 3 / 0 , .State > .
+rew < true, Error > .
+rew < 1 ==Bool 1, Error > .
 
-rew < x =  3 / 1 ; ,  x |-> 0 > .
+---
+--- rew < x =  3 / 1 ; ,  x |-> 0 > .
+---rew < x =  3 / 0 ; ,  x |-> 0 > .
 
-rew < x =  3 / 0 ; ,  x |-> 0 > .
-
-rew if( 1 <= 0 ) { x = 1 / 0; } else 1 . --- this should go to < 1 >
+---rew if( 1 <= 0 ) { x = 1 / 0 ; } else 1 . --- this should go to < 1 > .
 
 q

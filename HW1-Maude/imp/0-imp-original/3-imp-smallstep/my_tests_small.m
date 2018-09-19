9c05ea23592613436f390557949ef4cbdac490db
Brando Miranda <brando90@mit.edu>
2018-09-19 17:38:03 -0500

    progress

--- HW1-Maude/imp/0-imp-original/3-imp-smallstep/my_tests_small.m
+++ HW1-Maude/imp/0-imp-original/3-imp-smallstep/my_tests_small.m
@@ -13,6 +13,7 @@ mod TEST is including IMP-SEMANTICS-SMALLSTEP + IMP-PROGRAMS .  endm
 --- rew < 1 , .State > .
 --- rew < errorBool , .State > .
 rew o < 8 + 2 , .State > .
+rew o < ( 8 ) / ( 2 / 1 ) , .State > .
 rew * < ( 8 ) / ( 2 / 1 ) , .State > .
 rew * < ( 8 ) / ( 2 / 1 ) + ( 3 + ( 3 + 3 ) ) , .State > .
 
@@ -21,30 +22,5 @@ rew o < errorAr, x |-> 3 > .
 rew * < errorAr, x |-> 3 > .
 rew o < errorAr, .State > .
 rew * < errorAr, .State > .
-
-rew o < errorBool, x |-> 3 > .
-rew * < errorBool, x |-> 3 > .
-rew o < errorBool, .State > .
-rew * < errorBool, .State > .
-
-rew o < errorStmt, x |-> 3 > .
-rew * < errorStmt, x |-> 3 > .
-rew o < errorStmt, .State > .
-rew * < errorStmt, .State > .
-
---- SmallStep-Lookup
---- rew o < x, x |-> 3 > .
---- rew o < x, x |-> errorAr > .
-
---- SmallStep-ADD
-
---- SmallStep-DIV
---- rew o < 4 / 2 , .State > .
---- rew o < 4 / 0 , .State > .
---- rew o < ( 8 / 2) / ( 2 / 1 ) , .State > .
---- rew * < ( 8 / 2) / ( 2 / 1 ) , .State > .
---- rew o < errorAr / 1 , .State > .
---- rew o < ( 4 / 0 ) / 1 , .State > .
---- rew o < 4 / (1 / 0 ) , .State > .
 
 q
